23d698ba8739d45e027bddfdc415300aa6dcfe2f
tushar8590 <tushar8590@gmail.com>
2015-01-05 12:18:07 +0530

    this is second test commit

--- SpringIdol/src/main/java/com/springaction/springidol/MyMain.java
+++ SpringIdol/src/main/java/com/springaction/springidol/MyMain.java
@@ -15,8 +15,8 @@ public class MyMain {
 	public static void main(String[] args) throws Exception {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springaction/springidol/spring.xml");
 		//Performer performer = (Performer) context.getBean("poeticDuke");
-		Performer performer1 = (Performer) context.getBean("kenny");
-	    performer1.perform(); // calling the performer method    
+		Performer performer = (Performer) context.getBean("kenny");
+	    performer.perform(); // calling the performer method    
 
 	}
 
